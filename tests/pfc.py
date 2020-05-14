ce744622f357f146cad9c302c513a26e82f46b9f
Nick DeRobertis <whoopnip@gmail.com>
2020-05-14 09:07:25 -0400

    Expand example pyfileconf setup to have a filled out pipeline dict and actually load it

--- tests/pfc.py
+++ tests/pfc.py
@@ -1,14 +1,16 @@
 import os
 import shutil
+from typing import Callable
 from copy import deepcopy
 
 from pyfileconf import PipelineManager
 
 from tests.config import BASE_GENERATED_DIR
+from tests.input_files.amodule import a_function
 
 PM_FOLDER = os.path.join(BASE_GENERATED_DIR, 'pm')
 LOGS_PATH = os.path.join(PM_FOLDER, 'MyLogs')
-DEFAULTS_FOLDER_NAME = 'custom_defaults'
+DEFAULTS_FOLDER_NAME = 'defaults'
 
 PM_DEFAULTS = dict(
     folder=PM_FOLDER,
@@ -28,7 +30,6 @@ def create_pm(**kwargs) -> PipelineManager:
 def delete_pm_project(path: str = PM_FOLDER, logs_path: str = LOGS_PATH):
     all_paths = [
         os.path.join(path, 'defaults'),
-        os.path.join(path, 'custom_defaults'),
         os.path.join(path, 'pipeline_dict.py'),
         logs_path,
     ]
@@ -42,6 +43,27 @@ def delete_pm_project(path: str = PM_FOLDER, logs_path: str = LOGS_PATH):
             pass
 
 
+def pipeline_dict_str_with_obj(func: Callable, func_key: str, func_module: str) -> str:
+    return f'from {func_module} import {func.__name__}\n\npipeline_dict = {{\n\t"{func_key}": [{func.__name__}],\n}}\n'
+
+
+def nested_pipeline_dict_str_with_obj(func: Callable, section_key: str, func_key: str, func_module: str) -> str:
+    return f'from {func_module} import {func.__name__}\n\npipeline_dict = {{\n\t"{section_key}": {{\n\t\t"{func_key}": [{func.__name__}],\n\t}},\n}}\n'
+
+
+def write_a_function_to_pipeline_dict_file(pm_folder: str = PM_FOLDER, nest_section: bool = True):
+    file_path = os.path.join(pm_folder, 'pipeline_dict.py')
+
+    if nest_section:
+        write_str = nested_pipeline_dict_str_with_obj(
+            a_function, 'my_section', 'stuff', 'tests.input_files.amodule'
+        )
+    else:
+        write_str = pipeline_dict_str_with_obj(a_function, 'stuff', 'tests.input_files.amodule')
+    with open(file_path, 'w') as f:
+        f.write(write_str)
+
+
 if __name__ == '__main__':
     import argparse
 
@@ -53,4 +75,6 @@ if __name__ == '__main__':
     if args.delete:
         delete_pm_project()
     else:
-        create_pm()
+        write_a_function_to_pipeline_dict_file()
+        pm = create_pm()
+        pm.load()
