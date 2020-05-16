63b6edf27e41db9e0e3569b5b0848be49d713ff5
Nick DeRobertis <whoopnip@gmail.com>
2020-05-16 17:13:32 -0400

    Remove _general level from path

--- pyfileconfgui/pfc/extract.py
+++ pyfileconfgui/pfc/extract.py
@@ -8,12 +8,19 @@ from pyfileconf.views.object import ObjectView
 
 def full_dict_from_selector(s: Selector) -> dict:
     pc_dict = s._structure.copy()
+    out_dict = {}
     pc: PipelineCollection
     # TODO: special handling for _general
     for manager_key, manager_dict in pc_dict.items():
+        out_dict[manager_key] = {}
         for key, pc in manager_dict.items():
-            pc_dict[manager_key][key] = _pc_to_dict(pc)
-    return pc_dict
+            if key == '_general':
+                # Skip level of _general dict
+                out_dict[manager_key].update(_pc_to_dict(manager_dict['_general']))
+            else:
+                # Specific class dict, keep level
+                out_dict[manager_key][key] = _pc_to_dict(pc)
+    return out_dict
 
 
 def _pc_to_dict(pc: PipelineCollection) -> Union[dict, str]:
