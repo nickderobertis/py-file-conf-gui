6d937f26118ca08151e3cc246deda52d6fd779c8
Nick DeRobertis <whoopnip@gmail.com>
2020-05-17 17:26:44 -0400

    Update demo/test data to include class in pipeline dict as well as specific class dict

--- tests/pfc.py
+++ tests/pfc.py
@@ -7,21 +7,35 @@ from pyfileconf import PipelineManager
 
 from tests.config import BASE_GENERATED_DIR
 from tests.input_files.amodule import a_function
+from tests.input_files.mypackage.cmodule import ExampleClass
 
 PM_FOLDER = os.path.join(BASE_GENERATED_DIR, 'pm')
 LOGS_PATH = os.path.join(PM_FOLDER, 'MyLogs')
 DEFAULTS_FOLDER_NAME = 'defaults'
+EXAMPLE_CLASS_FILE_NAME = 'example_class_dict.py'
+EXAMPLE_CLASS_FILE_PATH = os.path.join(PM_FOLDER, EXAMPLE_CLASS_FILE_NAME)
+EC_CLASS_DICT = {
+    'class': ExampleClass,
+    'name': 'example_class'
+}
+CLASS_CONFIG_DICT_LIST = [
+    EC_CLASS_DICT
+]
 
 PM_DEFAULTS = dict(
     folder=PM_FOLDER,
     name='guipm',
     log_folder=LOGS_PATH,
     default_config_folder_name=DEFAULTS_FOLDER_NAME,
+    specific_class_config_dicts=CLASS_CONFIG_DICT_LIST,
 )
 
 
 def full_pm_setup(**kwargs) -> PipelineManager:
-    write_a_function_to_pipeline_dict_file()
+    if not os.path.exists(PM_FOLDER):
+        os.makedirs(PM_FOLDER)
+    write_to_pipeline_dict_file()
+    write_example_class_dict_to_file()
     pm = create_pm(**kwargs)
     pm.load()
     return pm
@@ -50,24 +64,35 @@ def delete_pm_project(path: str = PM_FOLDER, logs_path: str = LOGS_PATH):
             pass
 
 
-def pipeline_dict_str_with_obj(func: Callable, func_key: str, func_module: str) -> str:
-    return f'from {func_module} import {func.__name__}\n\npipeline_dict = {{\n\t"{func_key}": [{func.__name__}],\n}}\n'
-
-
-def nested_pipeline_dict_str_with_obj(func: Callable, section_key: str, func_key: str, func_module: str) -> str:
-    return f'from {func_module} import {func.__name__}\n\npipeline_dict = {{\n\t"{section_key}": {{\n\t\t"{func_key}": [{func.__name__}],\n\t}},\n}}\n'
-
-
-def write_a_function_to_pipeline_dict_file(pm_folder: str = PM_FOLDER, nest_section: bool = True):
+def write_to_pipeline_dict_file(pm_folder: str = PM_FOLDER, nest_section: bool = True):
     file_path = os.path.join(pm_folder, 'pipeline_dict.py')
 
-    if nest_section:
-        write_str = nested_pipeline_dict_str_with_obj(
-            a_function, 'my_section', 'stuff', 'tests.input_files.amodule'
-        )
-    else:
-        write_str = pipeline_dict_str_with_obj(a_function, 'stuff', 'tests.input_files.amodule')
+    write_str = """
+from tests.input_files.amodule import a_function
+from tests.input_files.mypackage.cmodule import ExampleClass
+
+pipeline_dict = {
+    "my_section": {"stuff": [a_function,], "woo": [ExampleClass,],},
+}
+"""
     with open(file_path, 'w') as f:
+        f.write(write_str)
+
+
+def class_dict_str(name: str, key: str, value: str) -> str:
+    return f'\n{name} = {{\n\t"{key}": ["{value}"],\n}}\n'
+
+
+def nested_class_dict_str(name: str, section_key: str, key: str, value: str) -> str:
+    return f'\n{name} = {{\n\t"{section_key}": {{\n\t\t"{key}": ["{value}"],\n\t}},\n}}\n'
+
+
+def write_example_class_dict_to_file(nest_section: bool = True):
+    if nest_section:
+        write_str = nested_class_dict_str('class_dict', 'my_section', 'stuff', 'data')
+    else:
+        write_str = class_dict_str('class_dict', 'stuff', 'data')
+    with open(EXAMPLE_CLASS_FILE_PATH, 'w') as f:
         f.write(write_str)
 
 
