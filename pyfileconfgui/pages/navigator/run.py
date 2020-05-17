fd1ee388bc8b05f47efe68f0c9b3ca187a2e0cd9
Nick DeRobertis <whoopnip@gmail.com>
2020-05-17 16:10:42 -0400

    Get basic version of streaming log output working, but doesn't work for quick runs

--- pyfileconfgui/pages/navigator/run.py
+++ pyfileconfgui/pages/navigator/run.py
@@ -1,8 +1,11 @@
+from io import StringIO
 from typing import Union, Sequence
+import sys
 
 import dash
 from dash.dependencies import Output, Input
 from dash.development.base_component import Component
+import dash_core_components as dcc
 import dash_html_components as html
 
 from pyfileconfgui.component import PFCGuiComponent
@@ -10,6 +13,8 @@ from pyfileconfgui.dash_ext.component import DashPythonComponent
 
 
 class RunEntryComponent(PFCGuiComponent):
+    is_running: bool = False
+    log_buffer: StringIO = StringIO()
 
     @property
     def layout(self) -> Sequence[Union['DashPythonComponent', Component]]:
@@ -17,6 +22,8 @@ class RunEntryComponent(PFCGuiComponent):
             html.H2('Running Item'),
             html.P(id='run-input'),
             html.Div(id='run-output'),
+            html.Div(id='run-interval-container'),
+            html.Div(id='run-console-output'),
         ]
 
     def add_callbacks(self, app: dash.Dash) -> None:
@@ -26,8 +33,42 @@ class RunEntryComponent(PFCGuiComponent):
             Output('run-output', 'children'),
             [Input('run-input', 'children')]
         )
+        self.add_callback(
+            app,
+            self.set_polling,
+            Output('run-interval-container', 'children'),
+            [Input('run-input', 'children'), Input('run-console-output', 'children')]
+        )
+        self.add_callback(
+            app,
+            self.stream_output,
+            Output('run-console-output', 'children'),
+            [Input('run-interval', 'n_intervals')]
+        )
         super().add_callbacks(app)
 
+    @property
+    def log_output(self) -> str:
+        return self.log_buffer.getvalue()
+
     def run_item(self, path: str):
+        self.reset_log()
+        self.is_running = True
+        orig_stdout = sys.stdout
+        sys.stdout = self.log_buffer
         output = self.gui.runner.run(path)
+        sys.stdout = orig_stdout
+        self.is_running = False
         return str(output)
+
+    def set_polling(self, path: str, run_output: str):
+        if self.is_running:
+            return dcc.Interval('run-interval', 200)
+        return None
+
+    def stream_output(self, n_intervals: int):
+        return dcc.Markdown(f'```python\n{self.log_output}\n```', style={'overflow': 'auto'})
+
+    def reset_log(self):
+        self.log_buffer.truncate(0)
+        self.log_buffer.seek(0)
